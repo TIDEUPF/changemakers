ef3544fecbc5e04a8ac7178023eefc16321f0f98
Pablo Abenia <pablo.abenia@upf.edu>
2017-11-21 13:57:03 +0100

    Added step 1 dialogues.

---
+++ assets/scripts/dialogs/resources/step1/step1_en.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "captain_d1": "My friend, you have to build a strong and truly resistant carriage! It should be safe for the royal family to travel in it. And no need for fancyness, there is beauty in roughness.",
+    "coachman_male_d1": "Long journeys can be exhausting. The carriage needs to be comfortable. And I'm not saying that only for me but also for the royal couple!",
+    "tailor_female_d1": "The carriage should be the richest one ever made in the kingdom in order to show the power of the king and wealth of its land, it should be a piece of art and luxury",
+    "butler_male_d1": "The royal family has a lot of stuff to carry around, clothes, gifts, etc. So I need space to put all that in the carriage",
+    "stable_boy_d1": "Think about the horses who will drag the carriage. If you want to go fast, you have to be small and light. Otherwise they'll get exhausted very quickly.",
+    "doctor_male_d1": "It would be great if the carriage could fit some of my medical instruments and drugs. I could have everything at my disposal in case of need",
+    "chef_female_d1": "The royal couple has to eat but can choose to stay in his other palaces or in guest´s houses. No need for an on-board mini kitchen.",
+    "civil_engineer_female_d1": "The carriage should be light and efficient. You are not building a tower that has to stand for centuries but a traveling cart. Make it light and agile.",
+    "merchant_male_d1": "Time is money my friend. A king and a queen have no time to waste wandering around. Just make it fast.",
+    "old_lady_female_d1": "Make that carriage comfortable because the cobble stones here are very hard... and the king and queen's butts will hurt pretty bad... I can assure you that",
+    "librarian_female_d1": "The carriage must be reflecting the importance of the role of the king throughout the world and the history of the kingdom and therefore be symbolically loaded with historical symbols, the carriage should become a legendary artefact.",
+    "butcher_male_d1": "Get out of my way, lad, there are some people working here!",
+    "potter_female_d1": "Don't let the royal family ruin the kingdom with this carriage. They already have much more than what we, the people, have. Make it simple.",
+    "vagabond_male_d1": "He who would travel happily must travel light.",
+    "huntress_female_d1": "I have no time to discuss. I have meat to catch."
+}
