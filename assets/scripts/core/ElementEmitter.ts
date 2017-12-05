0e0cf0909f2391a9c9f2a4f337ad5eca6293efbe
Pablo Abenia <pablo.abenia@upf.edu>
2017-12-05 15:11:08 +0100

    Improve GameElement constructor.

--- assets/scripts/core/ElementEmitter.ts
+++ assets/scripts/core/ElementEmitter.ts
@@ -1,3 +1,5 @@
-export default abstract class ElementEmitter {  
+export default abstract class ElementEmitter<T> {  
     abstract emitEvents(status: Object): Array<Object>;
+    constructor(element: T) {
+    }
 }  
--- assets/scripts/core/GameElement.ts
+++ assets/scripts/core/GameElement.ts
@@ -1,12 +1,13 @@
 import IGameElement from "./IGameElement";
 import ElementAction from "./ElementAction";
 import ElementEmitter from "./ElementEmitter";
+import * as actions from "./actions";
 
-export default class GameElement<T> extends IGameElement {  
+export default class GameElement<T> extends IGameElement {
     private elementStatus: Object;
     private elementAction: ElementAction<T>;
-    private elementEmitter: ElementEmitter;
-    private element: T;
+    private elementEmitter: ElementEmitter<T>;
+    private _element: T;
 
     private setElementStatus(status: Object): void {
         this.elementStatus = status;
@@ -16,19 +17,19 @@ export default class GameElement<T> extends IGameElement {
         this.elementAction = action;
     }
     
-    private setElementEmitter(emitter: ElementEmitter): void {
+    private setElementEmitter(emitter: ElementEmitter<T>): void {
         this.elementEmitter = emitter;
     }
 
     processAction(): void {
-        var actionResult = this.elementAction.processAction(this.elementStatus, this.element);
+        var actionResult = this.elementAction.processAction(this.elementStatus, this._element);
 
         if(actionResult.elementAction) {
             this.elementAction = actionResult.elementAction;
         }
 
-        if(actionResult.elementAction) {
-            this.elementAction = actionResult.elementAction;
+        if(actionResult.elementEmitter) {
+            this.elementEmitter = actionResult.elementEmitter;
         }
     }
 
@@ -36,8 +37,9 @@ export default class GameElement<T> extends IGameElement {
         this.elementEmitter.emitEvents(this.elementStatus);
     }
 
-    constructor(element: T) {
+    constructor(element: T, elementStatus: Object) {
         super();
-        this.element = element;
+        this._element = element;
+        this.setElementStatus(actions.actions[elementStatus["type"]][elementStatus["action"]]);
     }
 }
