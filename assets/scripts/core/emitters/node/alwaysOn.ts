23f64d5d93ccf87440192b664f9b2b9b8da71af6
Pablo Abenia <pablo.abenia@upf.edu>
2017-12-11 02:46:29 +0100

    Added default emitter.

---
+++ assets/scripts/core/emitters/node/alwaysOn.ts
@@ -0,0 +1,16 @@
+import ElementEmitter from "../../ElementEmitter";
+import EmitterResult from "../../EmitterResult";
+
+class AlwaysOnEmitter extends ElementEmitter<cc.Node> {
+    emitEvents(): EmitterResult {
+        let result: EmitterResult;
+        result = {};
+        console.log("alwaysOn emitter");
+        return result;
+    }
+}
+
+export const alarm: (elementStatus: Object, element: cc.Node) => ElementEmitter<cc.Node> = 
+    function(elementStatus: Object, element: cc.Node): ElementEmitter<cc.Node> {
+        return new AlwaysOnEmitter(elementStatus, element);
+    };
