7934de0d5d09014f92e4f20bec1c2bf37fe669f1
Pablo Abenia <pablo.abenia@upf.edu>
2017-12-15 09:26:13 +0100

    Hash directory elements.

--- assets/scripts/core/Directory.ts
+++ assets/scripts/core/Directory.ts
@@ -1,27 +1,82 @@
 export default class Directory {  
     private elements: Array<Object>;
+    private elementKeys: Object;
+    private elementId: Object;
 
     addElement(element: Object) : void {
         this.elements.push(element);
+
+        var addValueKey: (key: string, elementId: string) => void =
+            function(key: string, elementId: string): void {
+                if(typeof this.elementKeys[key] === "undefined") {
+                    this.elementKeys[key] = [];
+                }
+                this.elementKeys[key].push(elementId);
+            }
+
+        var addObject: (prefix: string, element: Object) => void =
+            function(prefix: string, element: Object): void {
+                for(var element_key in element) {
+                    if(element[element_key] instanceof Array) {
+                        for(var element_item of element[element_key]) {
+                            var value_key = prefix + element_key + ':' + element_item;
+                            addValueKey(value_key, element["id"]);
+                        }
+                    } else if(element[element_key] instanceof String) {
+                        var value_key = prefix + element_key + ':' + element[element_key];
+                        addValueKey(value_key, element["id"]);
+                    } else if(element[element_key] instanceof Object) {
+                        addObject(prefix + element_key + ':' + element_key + ':', element[element_key]);
+                    }
+                }
+            }
+            
+            addObject("", element);
+
+            this.elementId[element["id"]] = element;
     }
 
-    search(properties : Object) : Object {
-        var keys: Array<string> = ["attributes"];
+    search(properties : Object) : Array<Object> {
+        var keys: Array<string> = [];
+        var elementCount: Object;
+        var result: Array<Object>;
 
-        for(let keys_item of keys) {
-            if (!properties[keys_item])
-                continue;
-
-            switch(typeof properties[keys_item])
-            for(let event of this.eventList) {
-                if(element["type"] === event["type"]) {
-                    console.log("event notified");
-                    console.log(element);
-                    console.log(event);
+        var addObject: (prefix: string, element: Object) => void =
+        function(prefix: string, element: Object): void {
+            for(var element_key in element) {
+                if(element[element_key] instanceof Array) {
+                    for(var element_item of element[element_key]) {
+                        var value_key = prefix + element_key + ':' + element_item;
+                        keys.push(value_key);
+                    }
+                } else if(element[element_key] instanceof String) {
+                    var value_key = prefix + element_key + ':' + element[element_key];
+                    keys.push(value_key);
+                } else if(element[element_key] instanceof Object) {
+                    addObject(prefix + element_key + ':' + element_key + ':', element[element_key]);
                 }
             }
         }
+        
+        addObject("", properties);
 
+        for(var keys_item of keys) {
+            for(var elementId of keys_item) {
+                if(elementCount[elementId] === "undefined") {
+                    elementCount[elementId] = {};
+                }
+
+                elementCount[elementId][elementId] = true;
+            }
+        }
+
+        for(var elementCount_item in elementCount) {
+            if(Object.keys(elementCount[elementCount_item]).length === keys.length) {
+                result.push(this.elementId[elementCount_item]);
+            }
+        }
+
+        return result;
     }
 
     constructor() {
