c72e03a42d6502271afbb6c597491038cd5498e5
Pablo Abenia <pablo.abenia@upf.edu>
2018-05-02 14:00:37 +0200

    Added indicators to the notebook.

---
+++ assets/scripts/core/Notebook.ts
@@ -0,0 +1,14 @@
+import * as gd from "./GameData";
+import * as text from "../text/i18n";
+import {Scene} from "../core/Scene";
+import {Slider} from "../core/Slider";
+
+export const Notebook: Object = {
+    registerEvents: function() {
+
+    },
+    initIndicators: function() {
+
+    },
+    
+};
---
+++ assets/scripts/core/Slider.ts
@@ -0,0 +1,11 @@
+import * as gd from "./GameData";
+import * as text from "../text/i18n";
+import {Scene} from "../core/Scene";
+
+export const Slider: Object = {
+    setValue: function(slider_path, value) {
+        var node = gd.directory.getNode(slider_path);
+        var slider: cc.Slider = node.getComponent("cc.Slider");
+        slider.progress = value;
+    },
+};
