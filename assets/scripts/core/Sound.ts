197ece18aab01695ba78f43f237f3eb8729d885d
Pablo Abenia <pablo.abenia@upf.edu>
2018-05-23 14:08:44 +0200

    Added sound event play code.

--- assets/scripts/core/Sound.ts
+++ assets/scripts/core/Sound.ts
@@ -8,17 +8,25 @@ export const Sound = {
         var voices: Object = gd.directory.searchId("game_voices");
         cc.audioEngine.play(voices["data"][id]["path"], false, 1);
     },
+    play: function(id) {
+        return cc.audioEngine.play(id, false, 1);
+    },
     scene: function(data) {
         for(var trigger_event of data["events"]) {
             let event = JSON.parse(JSON.stringify(trigger_event));
+            event["listener"] = function(event) {
+                Sound.play(event["data"]["audio_id"]);
+            };
             event["data"] = {};
             for(var data_item in data) {
                 if(data_item === "events")
                     continue;
 
                 event[data_item] = data[data_item];
-                gd.observer.addEvent(event);
             }
+            
+            event["event"] = trigger_event;
+            gd.observer.addEvent(event);
         }
     }
 }
